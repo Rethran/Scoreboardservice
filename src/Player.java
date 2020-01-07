6300f877769d11580e3e1b5049dd0ca1e4b6ed98
Santeri <santtus97@hotmail.com>
2020-01-07 23:42:47 +0200

    added working list that compares scores
    added comparator list

--- src/Player.java
+++ src/Player.java
@@ -7,19 +7,19 @@ public class Player implements Comparable<Player>{
 		private Object getScore;
 //		private List<Player> players = new ArrayList<Player>();
 		
-		public Player() {
-			
-		}
-		
-		public Player (String name, int score) {
+		public Player(String name, int score) {
+			super();
 			this._name = name;
 			this._playerScore = score;
+			
+		}
+		public Player (String name, Integer score) {
+			
 			System.out.println("Player: " + name + " score: " + score);
 		}
 		public void addPlayer() {
 			
 		}
-		
 		public void setScore( String name, int score){
 			_playerScore = score;
 			System.out.println("Player: " + name + " score: " + score);
@@ -27,17 +27,17 @@ public class Player implements Comparable<Player>{
 		public Integer getScore() {
 			return _playerScore;
 		}
-		
-		
 		void changescore(int score) {
 		   System.out.println("passed score is: " + score);
 		}
-		
-		
+		public String getName() {
+			return _name;
+		}
+			
 		@Override
 		public int compareTo(Player compareScore) {
 			int compareGetScore = ((Player) compareScore).getScore();
-			// TODO Auto-generated method stub
+		
 			if(getScore == null) {
 				return 0;
 			}
@@ -45,7 +45,13 @@ public class Player implements Comparable<Player>{
 			
 			
 		}
-		
-	
-		
+		public static Comparator<Player> PlayerScoreComparator
+										= new Comparator<Player>() {
+			public int  compare (Player player1, Player player2) {
+				 Integer playerScore1 = player1.getScore();
+				 Integer playerScore2 = player2.getScore();
+				 return playerScore1.compareTo(playerScore2);
+			}
+			
+		};
 	}
--- src/paatiedosto.java
+++ src/paatiedosto.java
@@ -5,40 +5,25 @@ public class paatiedosto {
 	
 	 
 	public static void main(String[] args) {
-		
+	
 		List<Player> players = new ArrayList<Player>();
 		
-		Player myplayer =  new Player("Santeri", 100);
-		Player kiti =  new Player("Cindy", 2);
-		Player cindy =  new Player("Kiti", 300);
+		Player myplayer =  new Player("santeri", 100);
+		Player kiti =  new Player("cindy", 2);
+		Player cindy =  new Player("ti", 300);
 		Player noora =  new Player("noora", 150);
 		
 		players.add(noora);
 		players.add(myplayer);
 		players.add(kiti);
 		players.add(cindy);
+		Collections.sort(players,Player.PlayerScoreComparator);
 		
-	 Collections.sort(players);
-	 for(int i = 0; i < players.size(); i++) {
-		
-		System.out.println(players.get(i).getScore());
+		int i=0;
+	 for(Player temp: players) {
+		 
+		 System.out.println("Player"+ ++i + " : " + temp.getName()+"Score: "+ temp.getScore());
 		
 	 }
-	 
-	
-		
-	
-		
-		
-		
-			
-		
-		
-		
-		
-		
-		
 	}
-
-
 }
