bd362a32339551d58d29626673b01cb49e1bd0d3
Santeri <santtus97@hotmail.com>
2020-01-09 22:38:48 +0200

    finished
    Project is ready and finished the list and sortings on player.java file

--- src/Player.java
+++ src/Player.java
@@ -1,57 +1,85 @@
 import java.util.*;
 
+public class Player implements Comparable<Player> {
+	private Integer _playerScore;
+	private String _name;
+	private Object getScore;
+	private List<Player> _players;
 
-public class Player implements Comparable<Player>{
-		private Integer _playerScore;
-		private String _name;
-		private Object getScore;
-//		private List<Player> players = new ArrayList<Player>();
-		
-		public Player(String name, int score) {
-			super();
-			this._name = name;
-			this._playerScore = score;
-			
-		}
-		public Player (String name, Integer score) {
-			
-			System.out.println("Player: " + name + " score: " + score);
-		}
-		public void addPlayer() {
-			
-		}
-		public void setScore( String name, int score){
-			_playerScore = score;
-			System.out.println("Player: " + name + " score: " + score);
-		}
-		public Integer getScore() {
-			return _playerScore;
-		}
-		void changescore(int score) {
-		   System.out.println("passed score is: " + score);
-		}
-		public String getName() {
-			return _name;
-		}
-			
-		@Override
-		public int compareTo(Player compareScore) {
-			int compareGetScore = ((Player) compareScore).getScore();
-		
-			if(getScore == null) {
-				return 0;
-			}
-			return this._playerScore - compareGetScore;
-			
-			
-		}
-		public static Comparator<Player> PlayerScoreComparator
-										= new Comparator<Player>() {
-			public int  compare (Player player1, Player player2) {
-				 Integer playerScore1 = player1.getScore();
-				 Integer playerScore2 = player2.getScore();
-				 return playerScore1.compareTo(playerScore2);
-			}
-			
-		};
+	public Player(String name, int score) {
+		super();
+		this._name = name;
+		this._playerScore = score;
+
 	}
+
+	public Player() {
+
+	}
+
+	public Player(String name, Integer score) {
+
+		System.out.println("Player: " + name + " score: " + score);
+	}
+
+	public List<Player> getPlayerList() {
+
+		return new ArrayList<Player>(this._players);
+	}
+
+	public void setPlayer(String name, int score) {
+		_name = name;
+		_playerScore = score;
+		System.out.println("Player: " + name + " score: " + score);
+	}
+
+	public void setPlayerList(List<Player> list) {
+		this._players = new ArrayList<Player>(list);
+	}
+
+	public Integer getScore() {
+		return _playerScore;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String toString() {
+
+		return this._name + " " + this._playerScore;
+	}
+
+	public static String toString(List<Player> players) {
+		String s = "";
+		for (Player player : players) {
+			s += player + "\n";
+		}
+		return s;
+	}
+
+	@Override
+
+	public int compareTo(Player compareScore) {
+		int compareGetScore = ((Player) compareScore).getScore();
+
+		if (getScore == null) {
+			return 0;
+		}
+		return this._playerScore - compareGetScore;
+
+	}
+
+	public static Comparator<Player> PlayerScoreComparator = new Comparator<Player>() {
+		public int compare(Player player1, Player player2) {
+			Integer playerScore1 = player1.getScore();
+			Integer playerScore2 = player2.getScore();
+			return playerScore2.compareTo(playerScore1);
+		}
+
+	};
+}
--- src/paatiedosto.java
+++ src/paatiedosto.java
@@ -1,29 +1,26 @@
 import java.util.*;
+
+import com.sun.media.jfxmedia.events.PlayerEvent;
+
 public class paatiedosto {
 
-	
-	
-	 
 	public static void main(String[] args) {
-	
+
 		List<Player> players = new ArrayList<Player>();
 		
-		Player myplayer =  new Player("santeri", 100);
-		Player kiti =  new Player("cindy", 2);
-		Player cindy =  new Player("ti", 300);
-		Player noora =  new Player("noora", 150);
+		players.add(new Player("Peter", 10));
+		players.add(new Player("Pierre", 120));
+		players.add(new Player("George", 110));
+		players.add(new Player("John", 1255));
+	
+		Collections.sort(players, Player.PlayerScoreComparator);
 		
-		players.add(noora);
-		players.add(myplayer);
-		players.add(kiti);
-		players.add(cindy);
-		Collections.sort(players,Player.PlayerScoreComparator);
-		
-		int i=0;
-	 for(Player temp: players) {
-		 
-		 System.out.println("Player"+ ++i + " : " + temp.getName()+"Score: "+ temp.getScore());
-		
-	 }
+		System.out.println(Player.toString(players));
+		players.add(new Player("josu", 1235));
+
+		System.out.println("list with added persons\n");
+		Collections.sort(players, Player.PlayerScoreComparator);
+		System.out.println(Player.toString(players));
 	}
+
 }
